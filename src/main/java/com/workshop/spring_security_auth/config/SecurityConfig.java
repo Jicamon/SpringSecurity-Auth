bfc51357efc92becf6261a1123e0597866396831
Jicamon <chuy_1322@hotmail.com>
2024-12-03 17:07:15 -0700

    MyFirstCommit

---
+++ src/main/java/com/workshop/spring_security_auth/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.workshop.spring_security_auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/..").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+
+
+    }
+
+}
